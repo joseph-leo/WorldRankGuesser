09bf7610e166c390f4ac75b7970fe69fc09c8fab
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2023-06-14 23:21:39 -0500

    Add ice hockey ranking

--- WorldRankGuesser/Services/HockeyService.cs
+++ WorldRankGuesser/Services/HockeyService.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using System.Linq;
 using System.Reflection;
 using WorldRankGuesser.Data;
 using WorldRankGuesser.Helpers;
@@ -9,13 +10,12 @@ namespace WorldRankGuesser.Services
     {
         protected override List<HockeyRank> ParseRanks(HtmlDocument htmlDoc)
         {
-            switch (Sport)
+            return Sport switch
             {
-                case "Field Hockey":
-                    return ParseFieldHockey(htmlDoc);
-                default:
-                    return new List<HockeyRank>();
-            }
+                "Field Hockey" => ParseFieldHockey(htmlDoc),
+                "Ice Hockey" => ParseIceHockey(htmlDoc),
+                _ => new List<HockeyRank>(),
+            };
         }
 
         private List<HockeyRank> ParseFieldHockey(HtmlDocument htmlDoc)
@@ -37,6 +37,44 @@ namespace WorldRankGuesser.Services
                         Rank = rank,
                         Sport = Sport,
                         Gender = Gender
+                    });
+                }
+            }
+
+            return rankings;
+        }
+
+        private List<HockeyRank> ParseIceHockey(HtmlDocument htmlDoc)
+        {
+            List<HockeyRank> rankings = new();
+            var tables = htmlDoc.DocumentNode.SelectNodes("//table");
+
+            HtmlNode mensTable = tables[0];
+            rankings.AddRange(ParseIceHockeyTable(mensTable, "Men"));
+
+            HtmlNode womensTable = tables[1];
+            rankings.AddRange(ParseIceHockeyTable(womensTable, "Women"));
+
+            return rankings;
+        }
+
+        private List<HockeyRank> ParseIceHockeyTable(HtmlNode table, string gender)
+        {
+            List<HockeyRank> rankings = new();
+
+            foreach (var row in table.SelectSingleNode("tbody").SelectNodes("tr"))
+            {
+                List<string> cells = row.SelectNodes("td").Select(x => x.InnerText.Trim()).ToList();
+                string IOC = row.SelectNodes("td/a/span").Where(x => x.GetClasses().Contains("show-small")).FirstOrDefault().InnerText;
+
+                if (!rankings.Any(x => x.IOC == IOC) && int.TryParse(cells[0], out int rank))
+                {
+                    rankings.Add(new HockeyRank
+                    {
+                        IOC = IOC,
+                        Rank = rank,
+                        Sport = Sport,
+                        Gender = gender
                     });
                 }
             }
