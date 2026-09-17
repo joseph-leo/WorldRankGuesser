fad98a34d04d9946af338955a6e3b07c72145c9e
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2026-09-17 17:29:39 -0500

    Document the entry-row storage model

--- SportsRankingService.Tests/Persistence/RankingRepositoryTests.cs
+++ SportsRankingService.Tests/Persistence/RankingRepositoryTests.cs
@@ -1,7 +1,6 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Time.Testing;
-using SportsRankingService.Parsing;
 using SportsRankingService.Persistence;
 using SportsRankingService.Services;
 
